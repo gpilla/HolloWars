bd53d5fc832acd22227e0ce6f97b90fe35cc1e53
daniel.palazzo <dpalazzo@gmail.com>
2015-12-01 17:20:47 -0300

    emprolijado el game over

--- src/main/java/ar/edu/unq/hollowars/GameOverScene.java
+++ src/main/java/ar/edu/unq/hollowars/GameOverScene.java
@@ -4,13 +4,19 @@ import java.awt.Color;
 
 import com.uqbar.vainilla.GameScene;
 
+import ar.edu.unq.hollowars.components.ui.BackgroundGameOver;
+import ar.edu.unq.hollowars.components.ui.Image;
 import ar.edu.unq.hollowars.components.ui.Text;
 
 public class GameOverScene extends GameScene {
 	
 	@Override
 	public void onSetAsCurrent() {
-		this.addComponent(new Text("GAME OVER, PERDEDOR!", Color.BLACK, 400, 300 ));
+		this.addComponent(new Text("GAME OVER, PERDEDOR!", Color.BLACK, 545,480 ));
+//		this.addComponent(new BackgroundGameOver());
+		this.addComponent(new Image("images/holloAlpha.png",0.17, 00, 70, 0 ));
+		this.addComponent(new Image("images/strip.png",0.8, 300,400, -1 ));
+
 		super.onSetAsCurrent();
 	}
 	
